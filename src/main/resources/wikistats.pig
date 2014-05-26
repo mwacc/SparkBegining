f7556175de55d368ba51331b90ea6f2a97d4b0aa
Kostiantyn Kudriavtsev <kostiantyn_kudriavtsev@epam.com>
2014-05-26 14:46:22 +0300

    add coutner to wikistats example

--- src/main/resources/wikistats.pig
+++ src/main/resources/wikistats.pig
@@ -1,9 +1,18 @@
 A = LOAD 'hdfs://10.0.2.15:8020/user/hue/sparkin/' USING PigStorage(' ') AS (src: chararray, item:chararray, count: int, size:int);
+
+originalRecords = GROUP A ALL;
+originalRecNum = FOREACH originalRecords GENERATE COUNT(A) as allCount:double;
+
 filtered = FILTER A BY (src matches '^en.*');
 filtered2 = FOREACH filtered GENERATE item, count;
+filteredRecords = GROUP filtered2 ALL;
+filteredRecNum = FOREACH filteredRecords GENERATE COUNT(filtered2) as enCount:double;
 
 grouped = GROUP filtered2 BY item;
 counted = FOREACH grouped GENERATE group as item, SUM(filtered2.count) as count;
 ordered = ORDER counted BY count DESC;
 result = LIMIT ordered 50;
+
+dump filteredRecNum;
+dump originalRecNum;
 dump result;
--- src/main/scala/experiment/samples/WikiStats.scala
+++ src/main/scala/experiment/samples/WikiStats.scala
@@ -13,21 +13,25 @@ import experiment.{JobSample, SparkConfFactory}
  * Inspired by https://aws.amazon.com/articles/Elastic-MapReduce/4926593393724923
  *
  * Run with:
- *  java -jar SparkBegining-1.0-SNAPSHOT-shaded.jar experiment.samples.WikiStats hdfs://10.0.2.15:8020/user/hue/sparkin/pagecounts-20140401-130000.gz 2 cluster cluster
+ *  java -jar SparkBegining-1.0-SNAPSHOT-shaded.jar experiment.samples.WikiStats hdfs://10.0.2.15:8020/user/hue/sparkin/pagecounts-20140401-130000.gz 9 cluster cluster
  */
 class WikiStats extends SparkConfFactory with JobSample {
 
   override def runSample(args: Array[String]): Unit = {
     val input = args(0)
-    val nodesNum = args(1).toInt
+    val nodesNum = args(1).toInt // num of partitions on reduce stage, recommended to have 3-4 per CPU
     val sc = getConf(args)  // inherited method from my class to get Hadoop configuration
     val startTime: Long = System.currentTimeMillis();
 
     val file = sc.textFile(input)
 
-    val reducedList = file.
+    val originalRecNum =  file.count();
+    val enRecords = file.
       map(l => l.split("\\s")).
-      filter(l => l.length == 4 && l(0).toLowerCase().startsWith("en")).
+      filter(l => l.length == 4 && l(0).toLowerCase().startsWith("en"));
+
+    val enRecNum = enRecords.count();
+    val reducedList = enRecords.
       map(l => (l(1), l(2).toInt)).
       reduceByKey(_+_, nodesNum)
 
@@ -37,6 +41,9 @@ class WikiStats extends SparkConfFactory with JobSample {
     for( tuple <- sortedList ) {
       println( tuple._1 + " was accessed " + tuple._2 + "  times" )
     }
+    println( "\nAll records: %d; en records: %d".format(originalRecNum, enRecNum) )
+    println( String.format("En recodrs: %.2f %%\n",
+      new java.lang.Double( (enRecNum.toDouble/originalRecNum.toDouble)*100) ) )
   }
 
 }
